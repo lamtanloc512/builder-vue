ff9e1be19c39611d8a885184e12e95c2b6cdda39
lamtanloc512 <lamtanloc512@outlook.com>
2023-09-24 09:05:16 +0700

    continue

--- src/components/LayerManagerUI/LayerItem.vue
+++ src/components/LayerManagerUI/LayerItem.vue
@@ -9,7 +9,7 @@
 		editor: Editor | undefined;
 	}>();
 
-	const Layers = props.editor?.Layers;
+	const layers = props.editor?.Layers;
 
 	//states
 	const name = ref<string>();
@@ -18,14 +18,24 @@
 	const open = ref<boolean>();
 	const selected = ref<boolean>();
 	const hovered = ref<boolean>();
-	const editing = ref<boolean>();
+	// const editing = ref<boolean>();
 	const layerRef = ref();
 
-	const { componentResolverMap } = inject('layerManager');
+	const componentResolverMap: Map<string, Component> = new Map();
 
 	onMounted(() => {
-		updateLayer(Layers?.getLayerData(props.component));
-		componentResolverMap.set(layerRef.value, props.component);
+		updateLayer(layers?.getLayerData(props.component));
+		// props.editor?.on('layer:custom', (props) => {
+		// 	console.log(props);
+		// });
+
+		// Triggered when the root layer is changed.
+		// props.editor?.on('layer:root', (root) => {
+		// 	console.log(root);
+		// });
+
+		// Triggered when a component is updated, this allows you to update specific layers.
+		props.editor?.on('layer:component', onLayerComponentUpdate);
 	});
 
 	const updateLayer = (data: LayerData | undefined) => {
@@ -35,56 +45,55 @@
 		open.value = data?.open;
 		selected.value = data?.open;
 		hovered.value = data?.hovered;
+	};
 
-		// console.log(components.value);
+	const onLayerComponentUpdate = (_component: Component | undefined) => {
+		if (_component === props.component) {
+			updateLayer(layers?.getLayerData(_component));
+		}
 	};
 
 	const onDrop = () => {};
 
-	const convertToTreeNode = (components: Component[] | undefined) => {
-		if (!components) return [];
-		function convertChildNode(components: Component[]) {
-			console.log(components);
-
-			if ((components.length = 0)) return [];
-			function mapModelToChildNode(component: Component) {
-				return {
-					key: component.getId(),
-					title: component.getName(),
-				};
-			}
-
-			return components.map((el) => {
-				console.log(el);
-				return {
-					key: el.getId(),
-					title: el.getName(),
-					children: mapModelToChildNode(el),
-				};
-			});
-		}
-		return components?.map((el: Component) => {
+	const convertToTreeNode = (
+		components: Component[] | undefined,
+	): TreeNodeData[] => {
+		if (!components || components.length == 0) return [];
+		return components.map((el: Component): any => {
+			componentResolverMap.set(el.ccid, el);
 			return {
 				key: el.getId(),
 				title: el.getName(),
-				children:
-					el.collection.length > 0
-						? convertChildNode(el.collection.models)
-						: [],
+				children: convertToTreeNode(layers?.getComponents(el)),
 			};
 		});
+	};
+
+	const setSelected = (
+		selectedKeys: (string | number)[],
+		data: {
+			selected?: boolean | undefined;
+			selectedNodes: TreeNodeData[];
+			node?: TreeNodeData | undefined;
+			e?: Event | undefined;
+		},
+	) => {
+		const component = componentResolverMap.get(`${selectedKeys}`);
+		if (component) {
+			layers?.setLayerData(component, { selected: true }, { event: data.e });
+		}
 	};
 </script>
 
 <template>
 	<div>
 		<Tree
-			class="tree-demo"
 			draggable
 			blockNode
 			show-line
 			:data="components"
-			@drop="onDrop">
+			@drop="onDrop"
+			@select="setSelected">
 		</Tree>
 	</div>
 </template>
