59e57a3ece717e297375312071125a26c5cb8347
lamtanloc512 <lamtanloc512@outlook.com>
2023-10-02 19:51:40 +0700

    continue cleanup for build production version 0.1.2

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,60 @@
 <script setup lang="ts">
-	import {
-		Layout,
-		LayoutHeader,
-		LayoutContent,
-		LayoutSider,
-	} from '@arco-design/web-vue';
-	import { PanelUI, BlocksUI } from './components/index.ts';
-	import { provide, ref } from 'vue';
-	import AppLayout from './AppLayout.vue';
-	import { useAppStore } from './store/editorStore.ts';
-	const div = ref();
-	const store = useAppStore();
-	provide('editor', store.editor);
+import {
+	Layout,
+	LayoutHeader,
+	LayoutContent,
+	LayoutSider,
+} from '@arco-design/web-vue';
+import { PanelUI, BlocksUI } from './components/index.ts';
+import { provide, ref } from 'vue';
+import AppLayout from './AppLayout.vue';
+import { useAppStore } from './store/editorStore.ts';
+const div = ref();
+const store = useAppStore();
+provide('editor', store.editor);
 </script>
 
 <template>
 	<div>
 		<AppLayout>
-			<LayoutHeader
-				:style="{
-					height: `50px`,
-					width: '100%',
-					borderBottom: '1px solid var(--color-border-2)',
-					position: 'fixed',
-					zIndex: 100,
-				}">
+			<LayoutHeader :style=" {
+				height: `50px`,
+				width: '100%',
+				borderBottom: '1px solid var(--color-border-2)',
+				position: 'fixed',
+				zIndex: 100,
+			} ">
 			</LayoutHeader>
-			<Layout
-				:style="{
-					marginTop: '50px',
-					position: 'fixed',
-					top: 0,
-					left: 0,
-					right: 0,
-					bottom: 0,
-				}">
-				<LayoutSider
-					:style="{
-						height: '100%',
-						minWidth: '300px',
-						overflowX: 'auto',
-						borderRight: '1px solid var(--color-border-2)',
-					}">
+			<Layout :style=" {
+				marginTop: '50px',
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+			} ">
+				<LayoutSider :style=" {
+					height: '100%',
+					minWidth: '300px',
+					overflowX: 'auto',
+					borderRight: '1px solid var(--color-border-2)',
+				} ">
 					<BlocksUI />
 				</LayoutSider>
 				<LayoutContent>
-					<div
-						:ref="
-							(el) => {
-								div = el;
-								store.connect(div);
-							}
+					<div :ref=" (el) => {
+							div = el;
+							store.connect(div);
+						}
 						"></div>
 				</LayoutContent>
-				<LayoutSider
-					:style="{
-						minWidth: '260px',
-						height: '100%',
-						borderLeft: '1px solid var(--color-border-2)',
-						backgroundColor: 'var(--color-fill-2)',
-						padding: '8px',
-					}">
+				<LayoutSider :style=" {
+					minWidth: '260px',
+					height: '100%',
+					borderLeft: '1px solid var(--color-border-2)',
+					backgroundColor: 'var(--color-fill-2)',
+					padding: '8px',
+				} ">
 					<PanelUI />
 				</LayoutSider>
 			</Layout>
@@ -68,12 +62,4 @@
 	</div>
 </template>
 
-<style>
-	@import url('https://unpkg.com/grapesjs/dist/css/grapes.min.css');
-
-	.gjs-cv-canvas {
-		top: 0;
-		width: 100%;
-		height: 100%;
-	}
-</style>
+<style></style>
